5eb5a7faae096829168ef839769a8abdbb3b3b6e
Erkan IŞIK <erkanisik@yahoo.com>
2022-11-07 00:11:26 +0300

    Update and rename bison-3.7.4.sh to bison.sh

--- lfs-toolchain-builder/scripts/bison.sh
+++ lfs-toolchain-builder/scripts/bison.sh
@@ -2,13 +2,13 @@
 # 5.17. Bison-3.7.4
 set -e
 cd $LFS/sources
-rm -rf bison-3.7.4
-tar xf bison-3.7.4.tar.xz
-cd bison-3.7.4
+rm -rf bison-3.8.2
+tar xf bison-3.8.2.tar.xz
+cd bison-3.8.2
 
 ./configure --prefix=/tools
 make
 make install
 
 cd $LFS/sources
-rm -rf bison-3.7.4
+rm -rf bison-3.8.2
