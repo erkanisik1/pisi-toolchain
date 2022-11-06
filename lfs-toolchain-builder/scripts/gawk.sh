56f4d08c46942c3b6aa1d3f3342500dc75627b81
Erkan IŞIK <erkanisik@yahoo.com>
2022-11-07 00:19:45 +0300

    Update and rename gawk-5.1.0.sh to gawk.sh

--- lfs-toolchain-builder/scripts/gawk.sh
+++ lfs-toolchain-builder/scripts/gawk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# 5.23. Gawk-5.1.0
+# 5.23. Gawk-5.1.1
 set -e
 cd $LFS/sources
-rm -rf gawk-5.1.0
-tar xf gawk-5.1.0.tar.xz
+rm -rf gawk-5.1.1
+tar xf gawk-5.1.1.tar.xz
 cd gawk-5.1.0
 
 ./configure --prefix=/tools
@@ -11,4 +11,4 @@ make
 make install
 
 cd $LFS/sources
-rm -rf gawk-5.1.0
+rm -rf gawk-5.1.1
